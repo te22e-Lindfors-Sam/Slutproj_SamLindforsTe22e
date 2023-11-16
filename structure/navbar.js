f39631e2e6ce139f6304c260b6e17f554081ee3e
Sam Lindfors <sam.lindfors@elev.ga.ntig.se>
2023-11-16 14:01:48 +0100

    changed color for links and made js imporvments

--- structure/navbar.js
+++ structure/navbar.js
@@ -2,7 +2,7 @@ function openNav() {
   if (document.getElementById("ofCannvasNav").style.width === "250px"){//if the nav is already open close it
     closeNav();
   }
-  else{ 
+  else{ //other open the offcanvas nav
     document.getElementById("ofCannvasNav").style.width = "250px";
     setTimeout(() => {
       document.getElementById("closebtn").style.display = "block";
@@ -10,11 +10,13 @@ function openNav() {
   }
 }
 
+//close the nav
 function closeNav() {
     document.getElementById("ofCannvasNav").style.width = "0";
     document.getElementById("closebtn").style.display = "none";
 }
   
+//this runs when the site is open and calls the closenav wehn espace is pressed
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Escape') {
       closeNav();
